71342e6bbf22f659cacfdcea6ab7d4ea3aa8f8ec
Maureen Bockrath <maureenbockrath@Maureens-MacBook-Air.local>
2019-02-06 17:09:41 -0800

    server set up

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const cors = require('cors');
+var fs = require('file-system');
+const Schema = mongoose.Schema;
+
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(cors());
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/buddyup");
+
+// Start the API server
+app.listen(PORT, function () {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
